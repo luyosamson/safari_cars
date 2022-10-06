858f470e907483088c710b675a03daba19497b2c
luyosamson <luyosamson@gmail.com>
2022-10-06 14:07:10 +0300

    Creat data folder

---
+++ src/assets/data/blogData.js
@@ -0,0 +1,47 @@
+// import images from all-images/blog-img directory
+import img01 from "../all-images/blog-img/blog-1.jpg";
+import img02 from "../all-images/blog-img/blog-2.jpg";
+import img03 from "../all-images/blog-img/blog-3.jpg";
+
+const blogData = [
+  {
+    id: 1,
+    title: "The best way to drive cars",
+    author: "Samson Luyo",
+    date: "12 Sep, 2022",
+    time: "9pm",
+    imgUrl: img01,
+    description:
+      "The best ride I experienced",
+    quote:
+      "Voluptua dolore takimata dolor sadipscing nonumy consetetur et. Stet sed dolores dolores dolores dolores eos.",
+  },
+
+  {
+    id: 2,
+    title: "If your car battery is down",
+    author: "Mary Wanjiku",
+    date: "12 Sep, 2022",
+    time: "9pm",
+    imgUrl: img02,
+    description:
+      "The best moment with Safari Cars",
+    quote:
+      "Voluptua dolore takimata dolor sadipscing nonumy consetetur et. Stet sed dolores dolores dolores dolores eos.",
+  },
+
+  {
+    id: 3,
+    title: "The best way to give trip",
+    author: "Muhib",
+    date: "12 Dec, 2020",
+    time: "9pm",
+    imgUrl: img03,
+    description:
+      "What a ride!",
+    quote:
+      "Voluptua dolore takimata dolor sadipscing nonumy consetetur et. Stet sed dolores dolores dolores dolores eos.",
+  },
+];
+
+export default blogData;
---
+++ src/assets/data/serviceData.js
@@ -0,0 +1,45 @@
+const serviceData = [
+  {
+    id: 1,
+    title: "City Transfer",
+    icon: "ri-map-pin-2-line",
+    desc: "We offer quick and convenient transfers around the city on request and in just a few minutes.",
+  },
+
+  {
+    id: 2,
+    title: "Whole  City Tour",
+    icon: "ri-community-line",
+    desc: "We offer flexible tour around the city at your request",
+  },
+
+  {
+    id: 3,
+    title: "Unlimited Miles Car Rental",
+    icon: "ri-roadster-line",
+    desc: "You are free to tour with our car miles away from Nairobi",
+  },
+
+  {
+    id: 4,
+    title: "Fast & Easy Booking",
+    icon: "ri-timer-flash-line",
+    desc: "Our online platform offers flexible booking of our services",
+  },
+
+  {
+    id: 5,
+    title: "Many Pickup Locations",
+    icon: "ri-map-pin-line",
+    desc: "We offer several pickup location around the city",
+  },
+
+  {
+    id: 6,
+    title: "Airport Transfer",
+    icon: "ri-flight-takeoff-line",
+    desc: "We  offer quick and comfortable ride to airport.You Wont Miss The Flight",
+  },
+];
+
+export default serviceData;
