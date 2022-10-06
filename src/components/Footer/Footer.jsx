0d433aafcb971a398a52b8fff861a3a7fe4986aa
luyosamson <luyosamson@gmail.com>
2022-10-06 15:31:57 +0300

    Add quick links for navigation

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
 import { Link } from "react-router-dom";
 import "../../styles/footer.css";
+//This provides quick links to be accessed by users.
 
 const quickLinks = [
   {
