0dc1ae720c84f69c6ae6fe6ad7a6d477007e96e5
luyosamson <luyosamson@gmail.com>
2022-10-06 14:00:07 +0300

    Create a component folder

---
+++ src/components/UI/AboutSection.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import "../../styles/about-section.css";
+import aboutImg from "../../assets/all-images/cars-img/bmw-offer.png";
+
+const AboutSection = ({ aboutClass }) => {
+  return (
+    <section
+      className="about__section"
+      style={
+        aboutClass === "aboutPage"
+          ? { marginTop: "0px" }
+          : { marginTop: "280px" }
+      }
+    >
+      <Container>
+        <Row>
+          <Col lg="6" md="6">
+            <div className="about__section-content">
+              <h4 className="section__subtitle">About Us</h4>
+              <h2 className="section__title">Welcome to car rent service</h2>
+              <p className="section__description">
+              We are located along Lavington mall.We are glad you are here to know more
+              about us.We offer car hire services at an affordable rate with flexible terms 
+              </p>
+
+              <div className="about__section-item d-flex align-items-center">
+                <p className="section__description d-flex align-items-center gap-2">
+                  <i class="ri-checkbox-circle-line"></i> Affordable price
+                </p>
+
+                <p className="section__description d-flex align-items-center gap-2">
+                  <i class="ri-checkbox-circle-line"></i> Flexible terms and Conditions
+        
+                </p>
+              </div>
+
+              <div className="about__section-item d-flex align-items-center">
+                <p className="section__description d-flex align-items-center gap-2">
+                  <i class="ri-checkbox-circle-line"></i> Reliable cars for long distance journey
+                </p>
+
+                <p className="section__description d-flex align-items-center gap-2">
+                  <i class="ri-checkbox-circle-line"></i> Professional Drivers
+                </p>
+              </div>
+            </div>
+          </Col>
+
+          <Col lg="6" md="6">
+            <div className="about__img">
+              <img src={aboutImg} alt="" className="w-100" />
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default AboutSection;
